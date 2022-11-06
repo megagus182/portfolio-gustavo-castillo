b1be2dad1f4f5daa9f6ab1f6dcd64cc8093d1f21
megagus182 <megagus182@hotmail.com>
2022-11-06 15:22:47 -0600

    InstalandoChackraUI

--- src/App.js
+++ src/App.js
@@ -1,9 +1,32 @@
+import { BrowserRouter } from 'react-router-dom';
+import Home from "./Sections/Home"
+import AboutMe from "./Sections/AboutMe"
+import Projects from "./Sections/Projects"
+import ContactMe from "./Sections/ContactMe"
+import { ChakraProvider, extendTheme } from '@chakra-ui/react'
+import { MoonIcon } from '@chakra-ui/icons'
+
+const colors = {
+  brand: {
+    900: '#1a365d',
+    800: '#153e75',
+    700: '#2a69ac',
+  },
+}
+
+const theme = extendTheme({ colors })
 
 function App() {
   return (
-    <div className="App">
-      Portfolio
-    </div>
+    <ChakraProvider theme={theme}>
+    <BrowserRouter>
+    <MoonIcon/>
+				<Home />	
+        <AboutMe />	
+        <Projects />	
+        <ContactMe />	
+			</BrowserRouter>
+      </ChakraProvider>
   );
 }
 
