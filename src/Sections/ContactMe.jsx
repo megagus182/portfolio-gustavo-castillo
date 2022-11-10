741587d14febee3b8517ef5a8e0148e22fa63f98
megagus182 <megagus182@hotmail.com>
2022-11-09 20:34:44 -0600

    Adding ContactMe

--- src/Sections/ContactMe.jsx
+++ src/Sections/ContactMe.jsx
@@ -1,9 +1,53 @@
-
+import { Box, Image, Heading } from "@chakra-ui/react";
+import github from "../images/github.png";
+import linkedin from "../images/linkedin.png";
+import cv from "../images/cv.png";
 
 export default function ContactMe() {
-    return (
-      <div className="ContactMe">
-        ContactMe
-      </div>
-    );
-  }
+  return (
+    <Box width={"100%"} heigh={"100vh"} display={"flex"} className="Home">
+      {/* LEFT */}
+      <Box
+        display={"flex"}
+        flexDirection={"column"}
+        justifyContent={"center"}
+        width={"50%"}
+        marginLeft={"20px"}
+        className="leftSideFromHome"
+      >
+        <Heading marginTop={"20px"} as="h2" size="lg">
+        Contact Me
+      </Heading>
+        <Box>
+          <Image
+            backgroundColor={"red.600"}
+            height={"fit-content"}
+            width={"10%"}
+            src={github}
+            alt="github"
+          />
+        </Box>
+        <Box>
+          <Image
+          backgroundColor={"red.600"}
+            height={"fit-content"}
+            width={"10%"}
+            src={linkedin}
+            alt="linkedin"
+          />
+        </Box>
+        <Box >
+          <Image backgroundColor={"red.600"} height={"fit-content"} width={"10%"} src={cv} alt="cv" />
+        </Box>
+      </Box>
+      <Box width={"50%"} className="rightSideFromHome">
+        <Box
+          backgroundColor={"red.600"}
+          clipPath="polygon(25% 0%, 100% 0, 75% 100%, 0% 100%);"
+        >
+          FORMULARIO CONTROLADO
+        </Box>
+      </Box>
+    </Box>
+  );
+}
--- src/Sections/Projects.jsx
+++ src/Sections/Projects.jsx
@@ -1,4 +1,4 @@
-import { Box, Heading, Text, Image } from "@chakra-ui/react";
+import { Box, Heading, Text, Image, Highlight } from "@chakra-ui/react";
 import Food from "../images/CapturaFood.PNG";
 import Food2 from "../images/CapturaFood2.PNG";
 import Food3 from "../images/CapturaFood3.PNG";
@@ -8,12 +8,25 @@ export default function Projects() {
     return (
       <Box display={"flex"} flexDirection={"column"} alignItems={"center"} className="Projects">
         <Heading marginTop={"20px"}>These are some of my projects</Heading>
-
-
-        <Heading as="h2" size="lg">GameScript</Heading>
+<br></br>
+        <Heading as="h2" size="lg">
+        <Highlight
+    query={['GameScript']}
+    styles={{ px: '2', py: '1', rounded: 'full', bg: 'teal.400' }}
+  >
+          GameScript
+        </Highlight>
+          </Heading>
         <Text> GameScript</Text>
 
-        <Heading as="h2" size="lg">Food App</Heading>
+        <Heading as="h2" size="lg">
+        <Highlight
+    query={['FoodApp']}
+    styles={{ px: '2', py: '1', rounded: 'full', bg: 'teal.400' }}
+  >
+          FoodApp
+        </Highlight>
+          </Heading>
         <Image height={"fit-content"} width={"auto"} src={Food} alt="Food1" />
         <Image height={"fit-content"} width={"auto"} src={Food2} alt="Food2" />
         <Image height={"fit-content"} width={"auto"} src={Food3} alt="Food3" />
